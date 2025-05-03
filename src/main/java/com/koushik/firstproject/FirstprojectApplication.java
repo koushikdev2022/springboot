4dde61906d7fb54a099f4f8b8b77090f918438b4
koushikdev22 <koushik.journey.mondal@gmail.com>
2025-05-03 15:53:30 +0530

    new

--- src/main/java/com/koushik/firstproject/FirstprojectApplication.java
+++ src/main/java/com/koushik/firstproject/FirstprojectApplication.java
@@ -3,13 +3,22 @@ package com.koushik.firstproject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
+import org.springframework.data.mongodb.MongoTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
 @EntityScan(basePackages = "com.koushik.firstproject.model")
 public class FirstprojectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FirstprojectApplication.class, args);
 	}
-
+	@Bean
+    public PlatformTransactionManager add(MongoDatabaseFactory dbFactory){
+		return new MongoTransactionManager(dbFactory);
+	}
 }
