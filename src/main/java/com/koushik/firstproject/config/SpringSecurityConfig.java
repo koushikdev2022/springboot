8a1a6ded8f3656b0a9e766ef68e2f879f2b56ec1
koushikdev22 <koushik.journey.mondal@gmail.com>
2025-05-10 11:24:18 +0530

    new

--- src/main/java/com/koushik/firstproject/config/SpringSecurityConfig.java
+++ src/main/java/com/koushik/firstproject/config/SpringSecurityConfig.java
@@ -10,12 +10,16 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import com.koushik.firstproject.exception.CustomAuthenticationEntryPoint;
+import com.koushik.firstproject.filter.JwtFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SpringSecurityConfig {
+    @Autowired
+    private JwtFilter jwtFilter;
     @Autowired
 private CustomAuthenticationEntryPoint customAuthenticationEntryPoint;
     @Bean
@@ -33,8 +37,8 @@ private CustomAuthenticationEntryPoint customAuthenticationEntryPoint;
             .authenticationEntryPoint(customAuthenticationEntryPoint)
         )
             .formLogin(form -> form.permitAll()) // Enable form login and allow access to login page
-            .httpBasic(); // Enable HTTP Basic authentication
-
+            // .httpBasic(); // Enable HTTP Basic authentication
+          .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build(); // Build the SecurityFilterChain object
     }
 
