7c792695e8b5ed4d4cb3d379a6c65ee031f36ba4
koushikdev22 <koushik.journey.mondal@gmail.com>
2025-05-11 12:27:18 +0530

    new

---
+++ src/main/java/com/koushik/firstproject/dto/CharacterListDTO.java
@@ -0,0 +1,35 @@
+package com.koushik.firstproject.dto;
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CharacterListDTO {
+    private Long id;
+    private String responseDerective;
+    private String keyMemory;
+    private String exampleMessage;
+    private String videoUrl;
+    private Long userId;
+    private String characterUniqeId;
+    private String characterName;
+    private Date dob;
+    private String gender;
+    private String avatar;
+    private String backgroundStory;
+    private String characterGreeting;
+    private Integer parentId;
+    private Integer isActive;
+    private Integer isPublic;
+    private Integer isPublish;
+    private Integer type;
+    private Integer isCompleted;
+    private Integer isDeleted;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+}
